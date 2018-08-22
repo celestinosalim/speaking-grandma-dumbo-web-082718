112add32a1a65739b1d0efea987ccdc461ea9483
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 02:41:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,8 @@
-def speak_to_grandma(response)
- if response = response.upcase
-  if response == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif response == "WHAT DID YOU EAT TODAY?"
-    return "NO, NOT SINCE 1938!"
-  elsif response == "WHAT?"
-    return "NO, NOT SINCE 1938!"
-else
-  return "HUH?! SPEAK UP, SONNY!"
-end
-  end
-end
+def speak_to_grandma(phrase)
+# HUH?! SPEAK UP, SONNY!	 if phrase == "I LOVE YOU GRANDMA!"
+# unless you shout it (type in all capitals).	   return "I LOVE YOU TOO PUMPKIN!"
+   elsif phrase == phrase.upcase
+# If you shout, she can hear you (or at least she thinks so) 	   return "NO, NOT SINCE 1938!"
+# and yells back	  else phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+# NO, NOT SINCE 1938!	 end 
