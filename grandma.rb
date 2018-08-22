23c3f8ca00dc58596a0652f350bb52528d7e7e0b
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 02:33:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,4 @@
-def speak_to_grandma
-  
-  if speak_to_grandma == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
-  elsif speak_to_grandma == 'Hi Nana, how are you?'
-    puts 'HUH?! SPEAK UP, SONNY!'
-  elsif speak_to_grandma == 'Hi!'
-    puts 'HUH?! SPEAK UP, SONNY!'
-  elsif speak_to_grandma == 'Hi Nana, how are you?'
-    puts 'HUH?! SPEAK UP, SONNY!'
-  elsif speak_to_grandma == 'WHAT DID YOU EAT TODAY?'
-    puts "NO, NOT SINCE 1938!"
-  elsif speak_to_grandma == 'WHAT?'
-    puts "NO, NOT SINCE 1938!"
-  else
-    puts 'I LOVE YOU GRANDMA!'
+def speak_to_grandma(phrase)
+  if phrase.upcase == phrase
+    puts  'I LOVE YOU TOO PUMPKIN!'
   end
-end
