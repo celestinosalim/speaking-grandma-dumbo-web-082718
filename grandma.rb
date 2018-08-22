32dc8cebd141f87cbc5b449cd01f91d806600ef2
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 02:48:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!" || "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.capitalize
+  elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!'"
 
   end
