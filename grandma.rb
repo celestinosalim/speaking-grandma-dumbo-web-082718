7ea59b500a15b844fbff90b4484fca8751d9904d
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 02:14:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,15 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma
+
 if speak_to_grandma == 'I LOVE YOU GRANDMA!'
   return 'I LOVE YOU TOO PUMPKIN!'
-end
+elsif speak_to_grandma == 'Hi Nana, how are you?' || speak_to_grandma == 'Hi!' || speak_to_grandma == 'Hi Nana, how are you?'
+  return 'HUH?! SPEAK UP, SONNY!'
+elsif speak_to_grandma == 'WHAT DID YOU EAT TODAY?' || speak_to_grandma == 'WHAT?'
+  return "NO, NOT SINCE 1938!"
+else return 'I LOVE YOU GRANDMA!'
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
