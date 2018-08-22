e4a3f97e2f85d482819fb6638bd374bf02129bd8
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 02:16:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma
 
 if speak_to_grandma == 'I LOVE YOU GRANDMA!'
-  return 'I LOVE YOU TOO PUMPKIN!'
+   'I LOVE YOU TOO PUMPKIN!'
 elsif speak_to_grandma == 'Hi Nana, how are you?' || speak_to_grandma == 'Hi!' || speak_to_grandma == 'Hi Nana, how are you?'
-  return 'HUH?! SPEAK UP, SONNY!'
+   'HUH?! SPEAK UP, SONNY!'
 elsif speak_to_grandma == 'WHAT DID YOU EAT TODAY?' || speak_to_grandma == 'WHAT?'
-  return "NO, NOT SINCE 1938!"
+   "NO, NOT SINCE 1938!"
 else return 'I LOVE YOU GRANDMA!'
 
 end
